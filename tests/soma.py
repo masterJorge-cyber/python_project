067afe885a0eccf77d1db6f64bd7e24bb2a8ba5b
jorgeluiz <jorge.junior@brxretail.com>
2025-08-23 13:44:53 -0300

    soma.py

--- tests/soma.py
+++ tests/soma.py
@@ -1,11 +1,25 @@
+# x_total = 0
 
-def soma(x, y):
-    print(f'soma {int(x) + int(y)}')
+# def soma(x):
+#     global x_total
+#     x_total += int(x)
+#     print(f"Soma atual: {x_total}")
 
-print("Digite um valor de x:")
-x = input()
-print("Digite um valor de y:")
-y = input()
+# print("Digite um valor para soma, e -1 para sair:")
 
-soma(x, y)
+# while True:
+#     x = int(input())
+#     if x == -1:
+#         break
+#     soma(x)
 
+
+def soma(*numbers):
+    total = 0
+    for n in numbers:
+        total += n
+    print(f"Soma: {total}")
+
+
+soma(1, 2, 3, 4)   # Soma: 10
+soma(5, 10)        # Soma: 15
